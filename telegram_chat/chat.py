2f3f8684ab62e9bc64bbd30eeee4c355759c64a1
Bardo Espinosa <bardoespinosaa@gmail.com>
2021-02-07 19:34:25 -0600

    data parsing
    strings still need some work

--- telegram_chat/chat.py
+++ telegram_chat/chat.py
@@ -1,13 +1,33 @@
 import json
 import sqlite3
 
-j_data = open("chat.json", "rb")
-
-data = json.load(j_data)
-
 usr_lst = []
 msg_lst = []
-usr_set = set()
+
+j_data = open("chat.json", "rb")
+data = json.load(j_data)
+
+conn = sqlite3.connect("chat.db")# change the str to :memory: to work with memory db
+cur = conn.cursor() #cursor creator, commands go there
+cur.execute("""
+CREATE TABLE IF NOT EXISTS "Messages" (
+	"msg_id"	INTEGER NOT NULL UNIQUE,
+	"msg_date"	TEXT NOT NULL,
+	"from_id"	INTEGER NOT NULL,
+	"msg_txt"	TEXT,
+	PRIMARY KEY("msg_id")
+);
+""")
+cur.execute("""
+CREATE TABLE IF NOT EXISTS "Users" (
+	"usr_id"	INTEGER NOT NULL UNIQUE,
+	"usr_name"	INTEGER NOT NULL,
+	PRIMARY KEY("usr_id")
+);
+
+""")#msg table command AND usr table command
+
+
 
 for msg in data["messages"]:
     if msg["type"] == "message":
@@ -21,3 +41,23 @@ print(set(usr_lst))
 for item in msg_lst:
     print(item[1])
 """
+
+def insert_usr():
+    for usr in usr_lst:
+        try:
+            cur.execute(f"INSERT INTO Users ('usr_id', 'usr_name') VALUES('{usr[0]}','{usr[1]}')")
+            #conn.commit()
+        except Exception as e:
+            print()
+
+def insert_msg():
+    for msg in msg_lst:
+        try:
+            cur.execute(f"INSERT INTO Messages (msg_id, msg_date, from_id, msg_txt) VALUES({msg[0]}, '{msg[1]}', {msg[2]}, '{msg[3]}')")
+            #conn.commit()
+        except Exception as e:
+            print(e, msg[1])
+
+insert_msg()
+insert_usr()
+conn.commit()
